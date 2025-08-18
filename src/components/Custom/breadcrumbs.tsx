f69ae715345fe0f70a2bc1a8eedcd02421847434
egileekada <egileoniso.ekada@gmail.com>
2025-08-18 12:09:00 +0100

    update on event ui

---
+++ src/components/Custom/breadcrumbs.tsx
@@ -0,0 +1,49 @@
+"use client" 
+import useCustomTheme from "@/hooks/useTheme"
+import { IDonationList } from "@/models/donation"
+import { IEventType } from "@/models/Event"
+import { textLimit } from "@/utils/textlimiter"
+import { Flex, Text } from "@chakra-ui/react"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { IoIosArrowForward } from "react-icons/io"
+
+export default function BreadCrumbs(
+    {
+        eventName,
+        name
+    } : IEventType | IDonationList | any
+) {
+
+    const { back, push } = useRouter()
+    const { primaryColor } = useCustomTheme()
+
+    const query = useSearchParams();
+    const pathname = usePathname()
+    const edit = query?.get('edit');
+
+    const clickHandler = () => {
+        if(edit) { 
+            if(name) {
+                push("/product/fundraising")
+            } else {
+                push("/product/events")
+            }
+        } else {
+            if(pathname?.includes("event")) {
+                push("/product/events")
+            } else {
+                back()
+            }
+        }
+    }
+
+    return (
+        <Flex gap={"1"} alignItems={"center"} pb={"3"} >
+            <Text cursor={"pointer"} onClick={clickHandler} fontSize={"14px"} color={primaryColor} fontWeight={"500"} >Back</Text>
+            <IoIosArrowForward />
+            <Text fontSize={"14px"} fontWeight={"500"} >{name ? "Fundraising" : "Event"} details</Text>
+            <IoIosArrowForward />
+            <Text fontSize={"14px"} fontWeight={"500"} >{textLimit(name ? name : eventName, 20)}</Text>
+        </Flex>
+    )
+}
