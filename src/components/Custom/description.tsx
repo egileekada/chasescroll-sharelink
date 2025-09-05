780dad21246cf82e1e3b584b1ff611d5e458986a
egileekada <egileoniso.ekada@gmail.com>
2025-09-05 12:00:50 +0100

    refactor: update component styles and layout for improved readability
    - Adjust font sizes in DescriptionCard and EventLocation components for better visual hierarchy.
    - Modify layout properties in TicketSelection component to enhance responsiveness.
    - Clean up unused imports and commented code in Event view for better maintainability.

--- src/components/Custom/description.tsx
+++ src/components/Custom/description.tsx
@@ -12,8 +12,8 @@ export default function DescriptionCard({ description, limit, label, width } : {
 
     return ( 
         <Flex w={width ?? "full"} flexDir={"column"} bgColor={secondaryBackgroundColor} p={"4"} rounded={"16px"} gap={"2"} >
-            <Text fontSize={"14px"} fontWeight={"bold"} >{label}</Text>
-            <Text fontSize={"12px"} >{textLimit(capitalizeFLetter(description), textSize)}{description?.length > limit && (<span role='button' style={{ fontWeight: "700" }} onClick={() => setTextSize((prev) => prev === description?.length ? 100 : description?.length)} >{description?.length !== textSize ? "more" : "...less"}</span>)}</Text>
+            <Text fontSize={"15px"} fontWeight={"bold"} >{label}</Text>
+            <Text fontSize={"14px"} >{textLimit(capitalizeFLetter(description), textSize)}{description?.length > limit && (<span role='button' style={{ fontWeight: "700" }} onClick={() => setTextSize((prev) => prev === description?.length ? 100 : description?.length)} >{description?.length !== textSize ? "more" : "...less"}</span>)}</Text>
         </Flex>
     )
 }
--- src/components/Custom/eventLocation.tsx
+++ src/components/Custom/eventLocation.tsx
@@ -22,16 +22,16 @@ export default function EventLocation(
 
     return (
         <Flex w={"full"} gap={"2"} flexDir={"column"} >
-            <Flex maxW={showLink ? "400px" : "full"} rounded={"32px"} py={"2"} px={showLink? "3" : "0px"} bgColor={showLink ? secondaryBackgroundColor : "transparent"} w={"full"} gap={showLink ? "2" : "1"}>
+            <Flex maxW={showLink ? "600px" : "full"} rounded={"32px"} py={"2"} px={showLink? "3" : "0px"} bgColor={showLink ? secondaryBackgroundColor : "transparent"} w={"full"} gap={showLink ? "2" : "1"}>
                 <Flex w={"fit-content"} >
                 <LocationStroke />
                 </Flex>
-                <Text fontSize={fontSize ?? "14px"} fontWeight={"500"} whiteSpace={"none"} lineBreak={"anywhere"} >{data?.location?.toBeAnnounced ? "To Be Announced" : limit === 0 ? data?.location?.locationDetails : textLimit(data?.location?.locationDetails, limit)}</Text>
+                <Text fontSize={fontSize ?? ["12px", "12px", "14px"]} fontWeight={"500"} whiteSpace={"none"} lineBreak={"anywhere"} >{data?.location?.toBeAnnounced ? "To Be Announced" : limit === 0 ? data?.location?.locationDetails : textLimit(data?.location?.locationDetails, limit)}</Text>
             </Flex>
             {(data?.location?.link && showLink)&& (
                 <Flex maxW={["400px"]} rounded={"32px"} py={"2"} px={"3"} bgColor={secondaryBackgroundColor} w={"full"} gap={"2"} alignItems={"center"} >
                     <LinkIcon />
-                    <Text fontSize={"14px"} fontWeight={"500"} ><a target="_blank" href={data?.location?.link} >Click Me</a></Text>
+                    <Text fontSize={["12px", "12px", "14px"]} fontWeight={"500"} ><a target="_blank" href={data?.location?.link} >Click Me</a></Text>
                 </Flex>
             )}
         </Flex>
